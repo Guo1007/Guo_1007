e545c8e8d5a9d28493b252719fb910fe3978b3c8
Guo Chenyang <Chenyang@git.cn>
2026-05-20 10:55:09 +0800

    update

---
+++ furniture-vue/dist/assets/img-C1tFq-0j.js
@@ -0,0 +1,1 @@
+const r="https://gmc-1007.oss-cn-beijing.aliyuncs.com";function t(s,i=""){return s?s.startsWith("http")?s:(s.startsWith("/uploads/")&&(s=s.replace("/uploads","")),r+s):i}export{t as i};
